0ae68e7221714bd7ac7dcb1fcf7699126a674686
goyeahia <yeahia.ruet@gmail.com>
2020-04-27 13:30:08 +0600

    fixed

---
+++ yo.py
@@ -0,0 +1,12 @@
+from model import resolve_single
+from model.edsr import edsr
+
+from utils import load_image, plot_sample
+
+model = edsr(scale=4, num_res_blocks=16)
+model.load_weights('weights/edsr-16-x4/weights.h5')
+
+lr = load_image('demo/LR.png')
+sr = resolve_single(model, lr)
+
+plot_sample(lr, sr)
