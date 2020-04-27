5887ad3419a8fa53fcb17227944742e78488575b
goyeahia <yeahia.ruet@gmail.com>
2020-04-27 13:30:48 +0600

    fixed

--- utils.py
+++ utils.py
@@ -16,7 +16,7 @@ def plot_sample(lr, sr):
     titles = ['LR', f'SR (x{sr.shape[0] // lr.shape[0]})']
 
     for i, (img, title) in enumerate(zip(images, titles)):
-        plt.subplot(1, 2, i+1)
+        #plt.subplot(1, 2, i+1)
         plt.imshow(img)
         plt.title(title)
         plt.xticks([])
